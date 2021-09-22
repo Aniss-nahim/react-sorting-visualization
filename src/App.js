316e61f0b2f9787b0591e63fa94b33123832ebea
Aniss Nahim <aniss202nahim@gmail.com>
2021-09-22 22:20:11 +0200

    tailwind configuration using craco

---
+++ src/App.js
@@ -0,0 +1,15 @@
+const App = () => {
+  return (
+    <div className="h-screen bg-gray-50">
+      <div className="container">
+        <div className="row justify-center">
+          <h1 className="text-3xl font-medium">
+            React app sorting visualization
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      container: {
+        center: true,
+        padding: "2rem",
+      },
+    },
+  },
+  variants: {
+    extend: {
+      opacity: ["disabled"],
+    },
+  },
+  plugins: [require("@tailwindcss/forms")],
+};
